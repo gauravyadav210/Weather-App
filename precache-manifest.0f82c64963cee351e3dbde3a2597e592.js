98f022ba9757786ce9d88ad1723e5d1fc96b7368
gaurav kumar <gauravkumar@gauravs-MacBook-Air-2.local>
2024-04-14 02:49:33 +0530

    Updates

---
+++ precache-manifest.0f82c64963cee351e3dbde3a2597e592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b59e3f36954ee8c4ca947be9d582bce",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "8fa60c5fcccd8ed4d312",
+    "url": "/Weather-App/static/css/main.a29a900b.chunk.css"
+  },
+  {
+    "revision": "47692ce3783c3c0d7d84",
+    "url": "/Weather-App/static/js/2.21edb28f.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/Weather-App/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "8fa60c5fcccd8ed4d312",
+    "url": "/Weather-App/static/js/main.8b570159.chunk.js"
+  },
+  {
+    "revision": "17220609406415bfdc62",
+    "url": "/Weather-App/static/js/runtime~main.55a3cadd.js"
+  }
+]);
